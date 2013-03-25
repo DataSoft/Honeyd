5fca47f200cfd1aa5ff3681096750fed7c94b88c
David Clark <david.clark@datasoft.com>
2013-03-25 15:58:54 -0700

    Fix permissions problem with the database/folder created

--- scripts/lib/init.py
+++ scripts/lib/init.py
@@ -3,7 +3,17 @@ import os
 
 sys.path.append("/usr/share/honeyd/scripts/lib/")
 from names import InitializeDB
+from names import AddNames
 
 home = os.path.expanduser("~")
 InitializeDB(home + "/.config/honeyd/names")
-os.chmod(home + "/.config/honeyd/names", 0666)
+
+try:
+	os.chmod(home + "/.config/honeyd/names", 0666)
+except:
+	pass
+
+try:
+	os.chmod(home + "/.config/honeyd", 0777)
+except:
+	pass
