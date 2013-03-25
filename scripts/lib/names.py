c6fd2d2dec8df6d331b27e6a7e16d12f52ac1786
David Clark <david.clark@datasoft.com>
2013-03-25 16:03:55 -0700

    Fix some concurrency problems (multiple names assigned to same IP)

--- scripts/lib/names.py
+++ scripts/lib/names.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import sys
 
 #Returns the name that our IP address is allocated to
 #	returns empty string if not present
@@ -10,6 +11,7 @@ def GetAllocatedName(names_path, our_IP):
 	row = cursor.fetchone()
 	if(row is None):
 		return ""
+	conn.close()
 	return row[1].encode('ascii','ignore')
 
 #Picks the next name from the names db and allocates it to ourself
@@ -23,15 +25,29 @@ def AddNameAllocation(names_path, our_IP):
 	name = GetAllocatedName(names_path, our_IP)
 	if(name != ""):
 		return name
+	
+	# Get an exclusive lock on the database
+	conn.isolation_level = 'EXCLUSIVE'
+	conn.execute('BEGIN EXCLUSIVE')
+
+    # Some other script instance could have already got us a hostname. Check again now that we have exclusive lock.
+	cursor.execute("SELECT * FROM allocs WHERE IP=?", [our_IP])
+	row = cursor.fetchone()
+	if(row is not None):
+		conn.close()
+		return row[1].encode('ascii','ignore')
 		
 	#Get an unused name
 	cursor.execute("SELECT * FROM allocs WHERE IP is NULL")
 	row = cursor.fetchone()
 	if row is None:
+		conn.close();
+		sys.stderr.write("Unable to assign hostname to honeypot. No unused hostnames configured.\n")
 		return ""
 	name = row[1]
 	cursor.execute("UPDATE allocs SET IP=? WHERE name=?", [our_IP, name])
 	conn.commit()
+	conn.close()
 	return name.encode('ascii','ignore')
 
 #Add a list of new names to the names db
@@ -44,6 +60,7 @@ def AddNames(names_path, names):
 		except sqlite3.IntegrityError:
 			pass
 	conn.commit()
+	conn.close()
 
 def InitializeDB(names_path):
 	if not os.path.exists(os.path.dirname(names_path)):
@@ -53,12 +70,12 @@ def InitializeDB(names_path):
 	cursor.execute("select tbl_name from sqlite_master")
 	list_tables = cursor.fetchone()
 	if list_tables is None:
-		cursor.execute("CREATE TABLE allocs (IP text, name text PRIMARY KEY)")
+		cursor.execute("CREATE TABLE allocs (IP text UNIQUE, name text PRIMARY KEY)")
 		conn.commit()
 		return conn
 	list_tables = list_tables[0]
 	if("allocs" not in list_tables):
-		cursor.execute("CREATE TABLE allocs (IP text, name text PRIMARY KEY)")
+		cursor.execute("CREATE TABLE allocs (IP text UNIQUE, name text PRIMARY KEY)")
 	conn.commit()
 	return conn
 
