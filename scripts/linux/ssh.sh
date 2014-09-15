4f384519179cdba3874aca9c3f96faca67644bff
Kyle Luce <kyle.luce@datasoft.com>
2014-09-15 11:34:06 -0700

    Made ssh honey pot script simple
    - Objective is to send SSH version and sever connection quickly without waiting
      too long.
    
    - Waiting to long before was causing re-transmission of version etc that was
      parsed incorrectly by client.
       -> SSH error: Bad packet length 1397966893.
       -> This ( 1397966893) is "SSH-" in ASCII, by the way.
    
    - Removed echo $name, not sure what it was intended for, but was also causing
      client parse issues.
    
    - There will be no client login prompt for SSH as there is no SSL implemented in
      Honeyd yet.  This would be the only way to offer a prompt, after the
    	successful key exchange in the SSH protocol.
    
    Ticket: None (Pending?)  ssh.sh issue related.

--- scripts/linux/ssh.sh
+++ scripts/linux/ssh.sh
@@ -22,7 +22,34 @@ HOST="serv"
 
 my_start
 
-echo -e "$VERSION"
+#------------------------------------------------------------------
+# Objective for this script is to send the SSH/SSL version 
+#  And to disconnect quickly.  
+#
+#  There currently isn't any SSL implemented in Honeyd, do go get a prompt we'd
+# 	have to implement a basic SSL scheme here.
+#------------------------------------------------------------------
+echo -e "$VERSION\r\n"
+#echo -e "SSH-1.0-OpenSSH_1.2\r\n"
+sleep 1
+
+#------------------------------------------------------------------
+# For now comment this out to avoid corrupt packet message on clients
+#------------------------------------------------------------------
+
+#echo "Protocol mismatch."
+#sleep 1
+#
+#while read name; do
+#	echo "$name" >> $LOG
+#	LINE=`echo "$name" | egrep -i "[\n ]"`
+#	if [ -z "$LINE" ]; then
+#		echo "Protocol mismatch."
+#		my_stop	
+#	else
+#        echo "$name"
+#	fi
+#done
 
 while read name; do
 	echo "$name" >> $LOG
